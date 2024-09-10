92540f158e383ebab748827730f17fdf65f400fb
walminho <walmisson.cardoso073@gmail.com>
2024-09-10 01:16:43 -0300

    Config e conexão do BD

---
+++ database/migrations/2024_09_10_040756_create_tarefas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nulllable();
+            $table->enum('status', ['pendente', 'concluída'])->default('pendente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarefas');
+    }
+};
